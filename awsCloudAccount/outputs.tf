7b30b59d1d36fd7bd299d87d143689a9b2267233
Scott R. Bowe <scott@sentania.net>
2021-08-05 16:18:30 -0500

    outputs

--- awsCloudAccount/outputs.tf
+++ awsCloudAccount/outputs.tf
@@ -1,6 +1,6 @@
 output cloud_account {
   value = {
     "name" = vra_cloud_account_aws.this.name,
-    "id"   = vra_cloud_account_vsphere.this.id
+    "id"   = vra_cloud_account_aws.this.id
   }
 }
