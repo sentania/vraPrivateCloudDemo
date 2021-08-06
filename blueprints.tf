6d0dc94cfe031e0efa6c1a403023b080ee93f601
Scott R. Bowe <scott@sentania.net>
2021-08-06 11:30:08 -0500

    add bp

--- blueprints.tf
+++ blueprints.tf
@@ -28,3 +28,34 @@ module Private_Cloud_Sandbox_Simple_blueprint {
             - tag: "dynamicNetwork:Routed"
   EOT
 }
+
+module Private_Cloud_Sandbox_Simple_blueprint {
+    source           = "./blueprint"
+    blueprint_name = "Simple Blueprint2"
+    projectid = module.Private_Cloud_Sandbox.project.id
+    content = <<-EOT
+    formatVersion: 1
+    inputs: {}
+    version: 1.1.1
+    resources:
+      Cloud_Machine_1:
+        type: Cloud.Machine
+        properties:
+          image: CentOS7
+          flavor: Small
+          networks:
+            - network: "$${resource.Cloud_NSX_Network_1.id}"
+          customizationSpec: custSpec-CentOS7
+          tags:
+            - key: protection
+              value: bkupnoCred
+            - key: operatingSystem
+              value: centOS
+      Cloud_NSX_Network_1:
+        type: Cloud.NSX.Network
+        properties:
+          networkType: routed
+          constraints:
+            - tag: "dynamicNetwork:Routed"
+  EOT
+}
