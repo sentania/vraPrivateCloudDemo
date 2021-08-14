21901e80c3474fc96f664d21d9e925deaee26374
Scott R. Bowe <scottb@sentania.net>
2021-08-13 21:33:34 -0500

    fff

--- flavors.tf
+++ flavors.tf
@@ -50,8 +50,3 @@ module flv_labcomp02 {
     ]
     region_id = data.vra_region.flavor_region_labcomp02.id
 }
-
-data "vra_region" "flavor_region_cz_vmwareaws_us_east_2"{
-    region_id = module.cz_vmwareaws_us_east_2.cloud_zone.region_id
-    cloud_account_id = module.ca_vmwareaws.cloud_account.id
-}
