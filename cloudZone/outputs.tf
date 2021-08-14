1cd16f4208b8e258e981f5d1bca101bdbd2f84bc
Scott R. Bowe <scottb@sentania.net>
2021-08-13 21:04:57 -0500

    typo

--- cloudZone/outputs.tf
+++ cloudZone/outputs.tf
@@ -2,6 +2,6 @@ output cloud_zone {
   value = {
     "name" = vra_zone.this.name,
     "id"   = vra_zone.this.id
-    "region_id" = vra_zon.this.region_id
+    "region_id" = vra_zone.this.region_id
   }
 }
