4912b31d14fa85c036b66cf1baa00872119c562f
Scott R. Bowe <scott@sentania.net>
2021-08-20 11:25:46 -0500

    template_name

--- image_profile/main.tf
+++ image_profile/main.tf
@@ -1,6 +1,6 @@
 data "vra_image" "this" {
   count = length(var.image_mappings)
-  filter = "name eq '${var.image_mappings[count.index].image_name}' and cloudAccountId eq '${var.region.cloud_account_id}'"
+  filter = "name eq '${var.image_mappings[count.index].template_name}' and cloudAccountId eq '${var.region.cloud_account_id}'"
 }
 
 resource "vra_image_profile" "this" {
