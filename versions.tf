61b81d5211844464a03be8664ba980c42a535dc2
Scott R. Bowe <scottb@sentania.net>
2022-02-22 21:22:19 -0600

    .

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     vra = {
       source = "vmware/vra"
-      version = ">= 0.3.3"
+      version = ">= 0.5.0"
     }
     nsxt = {
       source = "vmware/nsxt"
