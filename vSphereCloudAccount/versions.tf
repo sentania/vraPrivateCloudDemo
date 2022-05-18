1488b358d44365c982195d272ff023c05530d0db
Scott R. Bowe <scott@sentania.net>
2022-05-18 14:38:24 -0500

    versions

--- vSphereCloudAccount/versions.tf
+++ vSphereCloudAccount/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     vra = {
       source = "vmware/vra"
-      version = ">= 0.3.3"
+      version = ">= 0.5.1"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.1.9"
 }
--- versions.tf
+++ versions.tf
@@ -1,4 +1,3 @@
-terraform {  }
-
-  required_version = ">= 1.0.4"
+terraform {
+  required_version = ">= 1.1.9"
 }
