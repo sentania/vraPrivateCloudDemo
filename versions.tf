f68eef7e710a8fe66df52838d33c33293e4acf04
Scott R. Bowe <scott@sentania.net>
2022-05-18 13:09:54 -0500

    clean providers

--- versions.tf
+++ versions.tf
@@ -9,10 +9,6 @@ terraform {
       version = ">= 3.1.0"
     }
     
-    vsphere = {
-      source = "hashicorp/vsphere"
-      version = ">= 1.24.2"
-    }
   }
   required_version = ">= 1.0.4"
 }
