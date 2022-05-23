7ebfad68993dcbad28109a4d6f763d7d7e3b78ce
Scott R. Bowe <scott@sentania.net>
2022-05-23 16:31:11 -0500

    .

--- cloudzone.tf
+++ cloudzone.tf
@@ -2,6 +2,9 @@ data "vra_region" "ca_vsphere_labcomp01"{
     region = module.ca_vsphere_labcomp01.enabled_regions[0].region
     cloud_account_id = module.ca_vsphere_labcomp01.cloud_account.id
 }
+data "vra_region" "ca_vsphere_labcomp01a"{
+    cloud_account_id = module.ca_vsphere_labcomp01.cloud_account.id
+}
 
 module cz_labcomp01 {
   source        = "./cloudZone"
