a7685f782660d5d1d4bb759bb2752da647f48362
Scott R. Bowe <scott@sentania.net>
2022-05-24 09:38:40 -0500

    .

--- cloudzone.tf
+++ cloudzone.tf
@@ -5,15 +5,14 @@ data "vra_region" "ca_vsphere_labcomp01"{
 }
 
 
-output "labcomp01a-dc" {
-    value = data.vra_region.ca_vsphere_labcomp01
-}
+local "labcomp01a-dc" {
+    value = matchkeys(values(data.vra_region.ca_vsphere_labcomp01), keys(data.vra_region.ca_vsphere_labcomp01.name),"lab-comp01")
 
 module cz_labcomp01 {
   source        = "./cloudZone"
   name          = "lab-comp01-cz"
   description   = "Lab Comp01 Datacenter"
-  region        =  data.vra_region.ca_vsphere_labcomp01["lab-comp01"].region
+  region        =  local.labcomp01a-dc.region
   capability_tags     = [
     {
       key   = "cloud",
