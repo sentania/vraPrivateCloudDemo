b88a308eaf8edf492b3b1b999f90be672cd447d1
Scott R. Bowe <scott@sentania.net>
2022-05-24 13:08:00 -0500

    .
    The key is determined based on what you passed to the for_each

--- cloudzone.tf
+++ cloudzone.tf
@@ -1,19 +1,19 @@
 data "vra_region" "ca_vsphere_labcomp01"{
-    for_each = { for dc in module.ca_vsphere_labcomp01.enabled_regions: dc.region => dc }
+    for_each = { for dc in module.ca_vsphere_labcomp01.enabled_regions: dc.name => dc }
     cloud_account_id = module.ca_vsphere_labcomp01.cloud_account.id
     region = each.value.region
 }
 
 
 locals {
-    ca_vsphere_labcomp01_region = matchkeys(values(data.vra_region.ca_vsphere_labcomp01), keys(data.vra_region.ca_vsphere_labcomp01.name),"lab-comp01")
+    ca_vsphere_labcomp01_region = data.vra_region.ca_vsphere_labcomp01["lab-comp01"].region
 }
 
 module cz_labcomp01 {
   source        = "./cloudZone"
   name          = "lab-comp01-cz"
   description   = "Lab Comp01 Datacenter"
-  region        =  locals.labcomp01a-dc.region
+  region        =  locals.labcomp01_region.region
   capability_tags     = [
     {
       key   = "cloud",
