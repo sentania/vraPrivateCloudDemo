f19d0bd8dc8f98921e9b782dc00ab7efbf3bdbc5
Scott R. Bowe <scott@sentania.net>
2022-05-24 09:40:15 -0500

    syntax

--- cloudzone.tf
+++ cloudzone.tf
@@ -7,7 +7,7 @@ data "vra_region" "ca_vsphere_labcomp01"{
 
 local "labcomp01a-dc" {
     value = matchkeys(values(data.vra_region.ca_vsphere_labcomp01), keys(data.vra_region.ca_vsphere_labcomp01.name),"lab-comp01")
-
+}
 module cz_labcomp01 {
   source        = "./cloudZone"
   name          = "lab-comp01-cz"
