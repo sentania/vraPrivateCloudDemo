f5cd0cf9394f6aafd6f465f125fff0196e5341b0
Scott R. Bowe <scott@sentania.net>
2022-05-25 13:49:50 -0500

    cloud zones

--- cloudzone.tf
+++ cloudzone.tf
@@ -4,17 +4,23 @@ data "vra_region" "ca_vsphere_labcomp01"{
     region = each.value.region
 }
 
+data "vra_region" "ca_vsphere_labcomp02"{
+    for_each = { for dc in module.ca_vsphere_labcomp02.enabled_regions: dc.name => dc }
+    cloud_account_id = module.ca_vsphere_labcomp02.cloud_account.id
+    region = each.value.region
+}
+
 
 locals {
-    ca_vsphere_labcomp01a_region = data.vra_region.ca_vsphere_labcomp01["lab-comp01"].id
-    ca_vsphere_labcomp01b_region = data.vra_region.ca_vsphere_labcomp01["lab-comp01b"].id
+    ca_vsphere_labcomp01_region = data.vra_region.ca_vsphere_labcomp01["lab-comp01"].id
+    ca_vsphere_labcomp02_region = data.vra_region.ca_vsphere_labcomp02["lab-comp02"].id
 }
 
 module cz_labcomp01 {
   source        = "./cloudZone"
   name          = "lab-comp01-cz"
   description   = "Lab Comp01 Datacenter"
-  region        =  local.ca_vsphere_labcomp01a_region
+  region        =  local.ca_vsphere_labcomp01_region
   capability_tags     = [
     {
       key   = "cloud",
@@ -23,11 +29,11 @@ module cz_labcomp01 {
   ]
 }
 
-module cz_labcomp01b {
+module cz_labcomp02 {
   source        = "./cloudZone"
-  name          = "lab-comp01b-cz"
-  description   = "Lab Comp01b Datacenter"
-  region        =  local.ca_vsphere_labcomp01b_region
+  name          = "lab-comp02-cz"
+  description   = "Lab Comp02 Datacenter"
+  region        =  local.ca_vsphere_labcomp02_region
   capability_tags     = [
     {
       key   = "cloud",
