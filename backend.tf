5cb6d18d17dd43018d61cc1dacf582fcd0bfdab4
Scott R Bowe <scottb@sentania.net>
2025-12-02 23:42:47 -0600

    update connection string

--- backend.tf
+++ backend.tf
@@ -1,5 +1,5 @@
 terraform {
   backend "pg" {
-    conn_str = "postgres://{var.backend_db_user}:{var.backend_db_pass}@{var.backend_db_host}:5433/{var.backend_db_name}?sslmode=disable"
+    conn_str = "postgres://${var.backend_db_user}:${var.backend_db_pass}@${var.backend_db_host}:5433/${var.backend_db_name}?sslmode=disable"
   }
 }
