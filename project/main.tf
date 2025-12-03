cab618118ab441f42c60a2e1426ed75565dc89c1
Scott R Bowe <scottb@sentania.net>
2025-12-03 10:58:48 -0600

    need to check property names

--- project/main.tf
+++ project/main.tf
@@ -6,10 +6,10 @@ resource "vra_project" "this" {
   administrators          = var.administrators
   operation_timeout       = 6000
   machine_naming_template = var.basename
-  dynamic "cloud_zone_assignments" {
+  dynamic "zone_assignments " {
     for_each = var.cloud_zone_ids
     content {
-      cloud_zone_id = cloud_zone_assignments.value
+      cloud_zone_id = zone_assignments .value
       priority      = 100
     }
   }
