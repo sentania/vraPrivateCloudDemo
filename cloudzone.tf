c662a83e1ec45fca2f25a44afa9adf079dcf8da5
Scott R Bowe <scottb@sentania.net>
2025-12-03 10:38:06 -0600

    tags to match

--- cloudzone.tf
+++ cloudzone.tf
@@ -6,7 +6,12 @@ module "cloud_zones" {
   name        = "${each.value.name}-cz"
   description = "Cloud zone for ${each.value.name}"
   region      = each.value.id
-
+  tags_to_match = [
+    {
+      key   = "vcfaCapablity"
+      value = "ready"
+    }
+  ]
   capability_tags = [
     {
       key   = "cloud"
