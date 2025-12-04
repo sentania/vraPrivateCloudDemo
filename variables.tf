cba6be5e938c190b1e2d6e285c797254f84860ea
Scott R Bowe <scottb@sentania.net>
2025-12-03 19:34:08 -0600

    update comments.

--- variables.tf
+++ variables.tf
@@ -2,50 +2,81 @@
 # General VCF-A Configuration
 ########################################
 
+/**
+ * vcfa_url
+ * URL of the VCF-A (Aria Automation) endpoint.
+ */
 variable "vcfa_url" {
   type = string
 }
 
+/**
+ * vcfa_refresh_token
+ * Refresh token used for authentication to the VCF-A API.
+ * Marked sensitive to avoid logging/output exposure.
+ */
 variable "vcfa_refresh_token" {
   type      = string
   sensitive = true
 }
 
+/**
+ * insecure
+ * Whether to skip SSL certificate verification when connecting
+ * to the VCF-A API (typically true for lab environments).
+ */
 variable "insecure" {
   type    = bool
   default = true
 }
 
+/**
+ * serviceAccountUserName
+ * Username for the VCF-A service account.
+ */
 variable "serviceAccountUserName" {
   type = string
 }
 
+/**
+ * serviceAccountPassword
+ * Service account password (sensitive).
+ */
 variable "serviceAccountPassword" {
   type      = string
   sensitive = true
 }
 
+
 ########################################
 # NSX Cloud Accounts
-#
-# Structure (map):
-#   nsx_accounts = {
-#     key = {
-#       name            = string
-#       hostname        = string
-#       capability_tags = list(object({
-#         key   = string
-#         value = string
-#       }))
-#     }
-#   }
 ########################################
 
+/**
+ * nsx_accounts
+ *
+ * A map of NSX cloud account definitions.
+ *
+ * Structure:
+ *   nsx_accounts = {
+ *     key = {
+ *       name            = string
+ *       hostname        = string
+ *       capability_tags = [
+ *         {
+ *           key   = string
+ *           value = string
+ *         }
+ *       ]
+ *     }
+ *   }
+ */
 variable "nsx_accounts" {
-  description = "Map of NSX cloud account definitions"
+  description = "Map of NSX cloud account definitions."
   type = map(object({
     name     = string
     hostname = string
+
     capability_tags = list(object({
       key   = string
       value = string
@@ -56,40 +87,46 @@ variable "nsx_accounts" {
 
 ########################################
 # vSphere Cloud Accounts
-#
-# Structure (map):
-#   vsphere_accounts = {
-#     key = {
-#       name                = string
-#       hostname            = string
-#       description         = string
-#       enabled_datacenters = list(string)
-#       nsxManager          = string
-#       capability_tags = list(object({
-#         key   = string
-#         value = string
-#       }))
-#       image_mappings = list(object({
-#         image_name    = string
-#         template_name = string
-#         cloud_config  = string
-#       }))
-#     }
-#   }
 ########################################
 
+/**
+ * vsphere_accounts
+ *
+ * A map of vSphere cloud account definitions.
+ *
+ * Structure:
+ *   vsphere_accounts = {
+ *     key = {
+ *       name                = string
+ *       hostname            = string
+ *       description         = string
+ *       enabled_datacenters = list(string)
+ *       nsxManager          = string
+ *       capability_tags     = [...]
+ *       image_mappings      = [
+ *         {
+ *           image_name    = string
+ *           template_name = string
+ *           cloud_config  = string
+ *         }
+ *       ]
+ *     }
+ *   }
+ */
 variable "vsphere_accounts" {
-  description = "Map of vSphere cloud account definitions"
+  description = "Map of vSphere cloud account definitions."
   type = map(object({
     name                = string
     hostname            = string
     description         = string
     enabled_datacenters = list(string)
     nsxManager          = string
+
     capability_tags = list(object({
       key   = string
       value = string
     }))
+
     image_mappings = list(object({
       image_name    = string
       template_name = string
@@ -98,8 +135,29 @@ variable "vsphere_accounts" {
   }))
 }
 
+
+########################################
+# Project Definitions
+########################################
+
+/**
+ * projects
+ *
+ * A map of project configurations.
+ *
+ * Structure:
+ *   projects = {
+ *     key = {
+ *       name           = string
+ *       description    = string
+ *       basename       = string
+ *       administrators = list(string)
+ *       infra_tag      = string
+ *     }
+ *   }
+ */
 variable "projects" {
-  description = "Map of project definitions"
+  description = "Map of project definitions."
   type = map(object({
     name           = string
     description    = string
